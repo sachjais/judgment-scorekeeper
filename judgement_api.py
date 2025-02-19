ba16ad2f99aef9c8dbbe4699f858833c0f08b176
sachjais <78134368+sachjais@users.noreply.github.com>
2025-02-19 22:41:28 +0530

    Update judgement_api.py

--- judgement_api.py
+++ judgement_api.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 import random
 
 app = Flask(__name__)
+CORS(app)  # This allows OpenAI to communicate with the API
+
 
 game_state = {}
 
